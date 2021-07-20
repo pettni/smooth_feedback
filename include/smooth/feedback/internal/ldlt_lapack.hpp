3a678923303799a0d5b8a88e1d5b7882e9bd8cd6
Petter Nilsson <pettni@umich.edu>
2021-07-20 15:12:49 -0400

    Use separate routines for factorization and solving

--- include/smooth/feedback/internal/ldlt_lapack.hpp
+++ include/smooth/feedback/internal/ldlt_lapack.hpp
@@ -3,27 +3,64 @@
 
 #include <lapacke.h>
 
+/**
+ * @file
+ * @brief Wrapper for lapack ldlt routinges
+ */
+
 namespace smooth::feedback::detail {
 
+// \cond
 template<typename Scalar>
 struct lapack_ldlt_fcn
 {
-  static constexpr auto value = &LAPACKE_ssysvx_work;
+  static constexpr auto factor = &LAPACKE_ssytrf_work;
+  static constexpr auto solve  = &LAPACKE_ssytrs_work;
 };
 
 template<>
 struct lapack_ldlt_fcn<double>
 {
-  static constexpr auto value = &LAPACKE_dsysvx_work;
+  static constexpr auto factor = &LAPACKE_dsytrf_work;
+  static constexpr auto solve  = &LAPACKE_dsytrs_work;
 };
+// \endcond
 
 /**
- * @brief Wrapper for LAPACKE xSYSVX routing for symmetric systems of equations.
+ * @brief Wrapper for LAPACKE xSYTRF / xSYTRS routines for factorizing and solving
+ * symmetric linear systems of equations.
  */
 template<typename Scalar, Eigen::Index N>
-  requires std::is_same_v<Scalar, float> || std::is_same_v<Scalar, double> class LDLTLapack
+  requires std::is_same_v<Scalar, float> || std::is_same_v<Scalar, double>
+class LDLTLapack
 {
 public:
+
+  /**
+   * @brief Factorize symmetric \f$ A \f$ to enable solving \f$ A x = b \f$.
+   *
+   * \p A is factorized as \f$ A = U D U^T \f$ where \f$ U \f$ is upper triangular
+   * and \f$ D \f$ is block-diagonal.
+   *
+   * @param A symmetric matrix to factorize.
+   *
+   * @note Only the upper triangular part of \f$ A \f$ is accessed.
+   */
+  inline LDLTLapack(Eigen::Matrix<Scalar, N, N> && A) : n_(A.cols()), AF_(std::move(A)), IPIV_(n_)
+  {
+    static constexpr lapack_int LWORK = N == -1 ? -1 : 2 * N;
+    Eigen::Matrix<Scalar, LWORK, 1> work(3 * N);
+
+    info_ = (*lapack_ldlt_fcn<Scalar>::factor)(LAPACK_COL_MAJOR,
+      'U',           // UPLO
+      n_,            // N
+      AF_.data(),    // A
+      n_,            // LDA
+      IPIV_.data(),  // IPIV
+      work.data(),
+      work.size());
+  }
+
   /**
    * @brief Factorize symmetric \f$ A \f$ to enable solving \f$ A x = b \f$.
    *
@@ -31,45 +68,41 @@ public:
    * and \f$ D \f$ is block-diagonal.
    *
    * @param A symmetric matrix to factorize
+   *
+   * @note Only the upper triangular part of \f$ A \f$ is accessed.
    */
   template<typename Derived>
   inline LDLTLapack(const Eigen::MatrixBase<Derived> & A)
-      : n_(A.cols()), A_(A), AF_(A.cols(), A.cols()), IPIV_(A.cols()),
-        work_(std::max<lapack_int>({1, 3 * n_, n_ * n_})), iwork_(n_)
-  {
-    Eigen::Matrix<Scalar, N, 1> b(n_), x(n_);
-    b.setZero();
-    Scalar rcond, ferr, berr;
-
-    (*lapack_ldlt_fcn<Scalar>::value)(LAPACK_COL_MAJOR,
-      'N',           // FACT: factor incoming matrix
-      'U',           // UPLO
-      n_,            // N
-      1,             // NRHS
-      A_.data(),     // A
-      n_,            // LDA
-      AF_.data(),    // AF
-      n_,            // LDAF
-      IPIV_.data(),  // IPIV
-      b.data(),      // B
-      n_,            // LDB
-      x.data(),      // X
-      n_,            // LDX
-      &rcond,
-      &ferr,
-      &berr,
-      work_.data(),
-      work_.size(),
-      iwork_.data());
-  }
+      : LDLTLapack(Eigen::Matrix<Scalar, N, N>(A))
+  {}
 
   /**
    * @brief Factorization status
    *
+   * @return integer \f$ i \f$
+   *
    * * 0: successful exit
-   * * i > 0: input matrix is singular with D(i, i) = 0.
+   * * \f$ i > 0 \f$: input matrix is singular s.t. \f$ D(i, i) = 0 \f$.
    */
   inline lapack_int info() const { return info_; }
+
+  /**
+   * @brief Solve linear symmetric system of equations in-place.
+   *
+   * @param[in, out] b in: right-hand side in \f$ A x = b \f$, out: solution \f$ x \f$
+   */
+  inline void solve_inplace(Eigen::Matrix<Scalar, N, 1> & b)
+  {
+    info_ = (*lapack_ldlt_fcn<Scalar>::solve)(LAPACK_COL_MAJOR,
+      'U',           // UPLO
+      n_,            // N
+      1,             // NRHS
+      AF_.data(),    // A
+      n_,            // LDA
+      IPIV_.data(),  // IPIV
+      b.data(),      // B
+      n_);           // LDB
+  }
 
   /**
    * @brief Solve linear symmetric system of equations.
@@ -78,43 +111,16 @@ public:
    */
   inline Eigen::Matrix<Scalar, N, 1> solve(const Eigen::Matrix<Scalar, N, 1> & b)
   {
-    Eigen::Matrix<Scalar, N, 1> x(n_);
-    Scalar rcond, ferr, berr;
-
-    info_ = (*lapack_ldlt_fcn<Scalar>::value)(LAPACK_COL_MAJOR,
-      'F',           // FACT: use existing factorization
-      'U',           // UPLO
-      n_,            // N
-      1,             // NRHS
-      A_.data(),     // A
-      n_,            // LDA
-      AF_.data(),    // AF
-      n_,            // LDAF
-      IPIV_.data(),  // IPIV
-      b.data(),      // B
-      n_,            // LDB
-      x.data(),      // X
-      n_,            // LDX
-      &rcond,
-      &ferr,
-      &berr,
-      work_.data(),
-      work_.size(),
-      iwork_.data());
-
+    Eigen::Matrix<Scalar, N, 1> x = b;
+    solve_inplace(x);
     return x;
   }
 
 private:
   const lapack_int n_;
-  const Eigen::Matrix<Scalar, N, N> A_;
-  Eigen::Matrix<Scalar, N, N> AF_;
+  Eigen::Matrix<Scalar, N, N, Eigen::ColMajor> AF_;
   Eigen::Matrix<lapack_int, N, 1> IPIV_;
   lapack_int info_;
-
-  static constexpr lapack_int LWORK = N == -1 ? -1 : std::max<lapack_int>({1, 3 * N, N * N});
-  Eigen::Matrix<Scalar, LWORK, 1> work_;
-  Eigen::Matrix<lapack_int, N, 1> iwork_;
 };
 
 }  // namespace smooth::feedback::detail
