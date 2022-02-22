1e955e2d246789a30033d35495a1385fd0a0fe57
pettni <pettni@users.noreply.github.com>
2022-02-22 15:42:57 +0000

    deploy: 377727cfea9923e5b0612f111a6628e012d4e422

--- search/functions_a.js
+++ search/functions_a.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['mark_5fexplicit_5fzeros_0',['mark_explicit_zeros',['../sparse_8hpp.html#a98aef964a93f8165ee540cd94e6df330',1,'smooth::feedback']]],
-  ['mesh_1',['Mesh',['../classsmooth_1_1feedback_1_1Mesh.html#adddac765aea6873c662d7a7a9f5ebc37',1,'smooth::feedback::Mesh']]],
+  ['mesh_1',['Mesh',['../classsmooth_1_1feedback_1_1Mesh.html#adddac765aea6873c662d7a7a9f5ebc37',1,'smooth::feedback::Mesh::Mesh()'],['../classsmooth_1_1feedback_1_1Mesh.html#ae1f16f27f80de2e5b6301fa683f19e3e',1,'smooth::feedback::Mesh::Mesh(const std::size_t n, const std::size_t k=Kmin)']]],
   ['mesh_5fdyn_2',['mesh_dyn',['../mesh__function_8hpp.html#a173d08faea4dc8d6910aa88693da82a5',1,'smooth::feedback']]],
   ['mesh_5fdyn_5ferror_3',['mesh_dyn_error',['../dyn__error_8hpp.html#a4f066c0d0b627af034009cbf5e3bf6d5',1,'smooth::feedback']]],
   ['mesh_5feval_4',['mesh_eval',['../mesh__function_8hpp.html#a171dc320f068226a8ad2efb20fce5ad3',1,'smooth::feedback']]],
