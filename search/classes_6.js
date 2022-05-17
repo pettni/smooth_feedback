49e25453bad2b1614662f7ba8ea28a6de12f4820
pettni <pettni@users.noreply.github.com>
2022-05-17 22:00:04 +0000

    deploy: 05ba02564799365ac3fafd8784c7eda32dfdc641

--- search/classes_6.js
+++ search/classes_6.js
@@ -1,6 +1,5 @@
 var searchData=
 [
   ['ocp_0',['OCP',['../structsmooth_1_1feedback_1_1OCP.html',1,'smooth::feedback']]],
-  ['ocp_3c_20x_2c_20u_2c_20smooth_3a_3afeedback_3a_3adetail_3a_3ampcobj_3c_20x_20_3e_2c_20smooth_3a_3afeedback_3a_3adetail_3a_3ampcdyn_3c_20t_2c_20x_2c_20u_2c_20f_2c_20diff_3a_3atype_3a_3adefault_20_3e_2c_20smooth_3a_3afeedback_3a_3adetail_3a_3ampcintegrand_3c_20t_2c_20x_2c_20u_20_3e_2c_20smooth_3a_3afeedback_3a_3adetail_3a_3ampccr_3c_20t_2c_20x_2c_20u_2c_20cr_2c_20diff_3a_3atype_3a_3adefault_20_3e_2c_20smooth_3a_3afeedback_3a_3adetail_3a_3ampcce_3c_20x_20_3e_20_3e_1',['OCP&lt; X, U, smooth::feedback::detail::MPCObj&lt; X &gt;, smooth::feedback::detail::MPCDyn&lt; T, X, U, F, diff::Type::Default &gt;, smooth::feedback::detail::MPCIntegrand&lt; T, X, U &gt;, smooth::feedback::detail::MPCCR&lt; T, X, U, CR, diff::Type::Default &gt;, smooth::feedback::detail::MPCCE&lt; X &gt; &gt;',['../structsmooth_1_1feedback_1_1OCP.html',1,'smooth::feedback']]],
-  ['ocpsolution_2',['OCPSolution',['../structsmooth_1_1feedback_1_1OCPSolution.html',1,'smooth::feedback']]]
+  ['ocpsolution_1',['OCPSolution',['../structsmooth_1_1feedback_1_1OCPSolution.html',1,'smooth::feedback']]]
 ];
