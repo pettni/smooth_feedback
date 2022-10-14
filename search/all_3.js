9f7362928c08403fb79b55311150125133a087ca
pettni <pettni@users.noreply.github.com>
2022-10-14 02:07:24 +0000

    deploy: a45deb6584ac781aa94b1608f1dd63b398f807c5

--- search/all_3.js
+++ search/all_3.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['d2f_0',['d2F',['../structsmooth_1_1feedback_1_1MeshValue_3_012_01_4.html#a2fd94a9575b5a26a850e62f5b3b56279',1,'smooth::feedback::MeshValue&lt; 2 &gt;']]],
-  ['d2r_5ffog_1',['d2r_fog',['../sparse_8hpp.html#a24b13a6fcf890dabd88a0cddb34fc816',1,'smooth::feedback']]],
+  ['d2r_5ffog_1',['d2r_fog',['../sparse_8hpp.html#a229d6c3ba248c80454412ca50076b642',1,'smooth::feedback']]],
   ['decrease_5fdegrees_2',['decrease_degrees',['../classsmooth_1_1feedback_1_1Mesh.html#a087863f243124a3f83a1714db9beef8c',1,'smooth::feedback::Mesh']]],
   ['delta_3',['delta',['../structsmooth_1_1feedback_1_1QPSolverParams.html#af0f769c1e55ac5f9ef8ab1c7e46bb31a',1,'smooth::feedback::QPSolverParams']]],
   ['df_4',['dF',['../structsmooth_1_1feedback_1_1MeshValue_3_011_01_4.html#a2c865be96aa377c9d6aaa6b34e91bb65',1,'smooth::feedback::MeshValue&lt; 1 &gt;']]],
--- search/functions_3.js
+++ search/functions_3.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['d2r_5ffog_0',['d2r_fog',['../sparse_8hpp.html#a24b13a6fcf890dabd88a0cddb34fc816',1,'smooth::feedback']]],
+  ['d2r_5ffog_0',['d2r_fog',['../sparse_8hpp.html#a229d6c3ba248c80454412ca50076b642',1,'smooth::feedback']]],
   ['decrease_5fdegrees_1',['decrease_degrees',['../classsmooth_1_1feedback_1_1Mesh.html#a087863f243124a3f83a1714db9beef8c',1,'smooth::feedback::Mesh']]]
 ];
