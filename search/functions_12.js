fc834618804a8f87756704d4cb44592327f1963a
pettni <pettni@users.noreply.github.com>
2022-12-23 15:10:43 +0000

    deploy: 9a08971500bbdb36e59feec209b60e247c7ff145

--- search/functions_12.js
+++ search/functions_12.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['_7empc_0',['~MPC',['../classsmooth_1_1feedback_1_1MPC.html#a28d8c2d7111795cc0fe9de527753af97',1,'smooth::feedback::MPC']]],
-  ['_7epid_1',['~PID',['../classsmooth_1_1feedback_1_1PID.html#af4c37464a7349a495eccfab1c964dd8c',1,'smooth::feedback::PID']]]
+  ['_7epid_1',['~PID',['../classsmooth_1_1feedback_1_1PID.html#af4c37464a7349a495eccfab1c964dd8c',1,'smooth::feedback::PID']]],
+  ['_7eqpsolver_2',['~QPSolver',['../classsmooth_1_1feedback_1_1QPSolver.html#aa2e9d09704009eab6c24e1e3f22ece3f',1,'smooth::feedback::QPSolver']]]
 ];
